8d390f230c12f8669da23048576d8c0fb07bfa1e
anakais <alexandre.saito@outlook.com>
2021-11-22 20:53:14 -0300

    Update colors on tw.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,26 @@ module.exports = {
   purge: ['./src/**/*.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        red: {
+          900: '#683545',
+          300: '#F9C5B3',
+          100: '#F0E2DA'
+        },
+        gray: {
+          500: '#6C6C6C',
+          200: '#F9F9F9'
+        },
+        orange: {
+          500: '#F2BF5E',
+          800: '#D99543'
+        },
+        yellow: {
+          200: '#F2DD72'
+        }
+      }
+    },
   },
   variants: {
     extend: {},
